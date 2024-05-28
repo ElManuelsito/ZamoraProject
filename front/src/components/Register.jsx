3531ee8756a97e8fce07dc9af74ef1755f783849
Manuel <a.monsalve@alumno.um.edu.ar>
2024-05-28 18:56:34 -0300

    Rename folders (and caused a lot of chaos)

---
+++ front/src/components/Register.jsx
@@ -0,0 +1,72 @@
+import { Formik, Form, Field } from 'formik'
+import axios from 'axios'
+
+export const Register = () => {
+
+    const initialValues = {
+        name: '',
+        email:'',
+        password:''
+    };
+
+  const handleRegister = async (values) => {
+    console.log('valores desde el front', values)
+    // try {
+    //     const response = await axios.post('https://localhost:5000/auth/register', values);
+    //     console.log(response.data);
+    // }  catch (error) {
+    //     console.error(error);
+    // }
+    const response = await axios.post('https://localhost:5000/auth/register', values);
+    console.log(response.data);
+  };
+
+  return (
+    <>
+      <div class="row justify-content-center">
+        <div class="col-md-4">
+            <h1 class="services_text custom_main">Registro</h1>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={handleRegister}
+            >
+                <Form>
+                    <div class="form-floating">
+                        <Field 
+                            type="text"
+                            class="form-control"
+                            id="floatingName"
+                            placeholder="Name"
+                            name="name"
+                        />
+                        <label htmlFor="floatingName">Nombre</label>
+                    </div>
+                    <div class="form-floating">
+                        <Field 
+                            type="text"
+                            class="form-control"
+                            id="floatingInput"
+                            placeholder="Email"
+                            name="email"
+                        />
+                        <label htmlFor="floatingInput">Correo</label>
+                    </div>
+                    <div class="form-floating">
+                        <Field 
+                            type="text"
+                            class="form-control"
+                            id="floatingPassword"
+                            placeholder="Password"
+                            name="password"
+                        />
+                        <label htmlFor="floatingPassword">Contraseña</label>
+                    </div>
+                    {/* <button class="btn btn-primary w-100 py-2" type="submit" onClick={handleRegister}>Registrarse</button> */}
+                    <button class="btn btn-primary w-100 py-2" type="submit">Registrarse</button>
+                </Form>
+            </Formik>
+        </div>
+      </div>
+    </>
+  )
+}
